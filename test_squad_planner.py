c17d957c481a25baeae122b7cf9f512363f59f82
md10ktech <147039519+md10ktech@users.noreply.github.com>
2024-01-22 18:00:54 +0800

    Squad Planner some interfaces.

--- test_squad_planner.py
+++ test_squad_planner.py
@@ -10,7 +10,14 @@ user_agent = ("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.3
               "Safari/537.36")
 
 log_er = Logger("SquadPlanner")
-token = login(email="brazil@email.com", password="Password123")["response"]
+token = login(email="yes@yes.com", password="123456Aa")["response"]
+common_request = {"squadType": "null", "setIndex": 0,
+                  "squadPosition": {"position": 0, "soccerPlayerId": "0"}, "playerPosition": "null"}
+
+
+def get_player_template(role_position, player_id, role):
+    return {"squadPosition": {"position": role_position, "soccerPlayerId": player_id},
+            "playerPosition": role}
 
 
 def get_all_soccer_players_info():
@@ -21,8 +28,8 @@ def get_all_soccer_players_info():
                                      'Authorization': token})
     if response.status_code == 200:
         log_er.log_info(f" -Get All Soccer Players Successful.")
-        # json_str = json.dumps(response.json())
-        # pyperclip.copy(json_str)
+        json_str = json.dumps(response.json())
+        pyperclip.copy(json_str)
     else:
         log_er.log_info(f" -Get All Soccer Players failed. Error: {response.json()}")
     return response.json()
@@ -35,7 +42,6 @@ def get_squad_info():
                             headers={'Content-Type': 'application/json', 'User-Agent': user_agent,
                                      'Authorization': token})
     if response.status_code == 200:
-        log_er.log_info(f" -Get Squad Information Successful.")
         json_str = json.dumps(response.json())
         pyperclip.copy(json_str)
     else:
@@ -48,48 +54,81 @@ def get_all_soccer_player_ids():
     soccer_player_ids = []
     all_soccer_players_json = get_all_soccer_players_info()
     for player in all_soccer_players_json:
-        soccer_player_ids.append(player["id"])
+        soccer_player_ids.append((player["id"], player["playerPosition"]))
     log_er.log_info(f" {soccer_player_ids}")
     return soccer_player_ids
 
 
+def remove_slot(squad_type, preset, role_position, player_id, role):
+    """ 6.7 Remove Formation Soccer Slot - Actually, completely removes a slot from the squad."""
+    response = requests.put(url=domain + '/api/v1/squad/formation/remove-soccer-slot',
+                            headers={'User-Agent': user_agent, 'Authorization': token},
+                            json={"squadType": squad_type, "setIndex": preset,
+                                  "squadPosition": {"position": role_position, "soccerPlayerId": player_id},
+                                  "playerPosition": role})
+    if response.status_code == 400:
+        log_er.log_info(response.json())
+    return response.status_code
+
+
+def test_remove_slot():
+    """ Remove A Player Slot"""
+    # 1. Able to remove with proper input
+    # status = remove_slot("HOME", 0, 1, "956177718774591488", "FW")
+    # 2. Able to remove with player id = 0
+    # status = remove_slot("HOME", 0, 2, "0", "FW")
+    # 3. Able to remove with a player id elsewhere in the squad
+    # status = remove_slot("HOME", 0, 1, "956177718787174401", "MID")
+    # 4. Able to remove with non-existent player id
+    # status = remove_slot("HOME", 0, 1, "90123456789", "DEF")
+    # 5. Able to remove with role position outside range - returns success!
+    status = remove_slot("HOME", 0, 5, "0", "FW")
+
+    get_squad_info()
+    assert status == 200
+
+
 def autofill_4_4_2():
     """ 6.9 auto fill formation slot. """
-    items = []
-    slot = {}
-    increment = 0
+    position = 1
+    # player_ids = []
+    players = []
     all_soccer_players_json = get_all_soccer_players_info()
+
+    # logic difficulty with dictionaries
     for player in all_soccer_players_json:
-        increment += 1
-        if player["playerPosition"] == "FW":
-            if increment <= 2:
-                slot["position"] = increment
-                slot["soccerPlayerId"] = player["id"]
-                items.append({"squadPosition": slot, "playerPosition": "FW"})
-            else:
-                break
-        elif player["playerPosition"] == "MID":
-            if increment <= 6:
-                slot["position"] = increment
-                slot["soccerPlayerId"] = player["id"]
-                items.append({"squadPosition": slot, "playerPosition": "MID"})
-        elif player["playerPosition"] == "DEF":
-            if increment <= 10:
-                slot["position"] = increment
-                slot["soccerPlayerId"] = player["id"]
-                items.append({"squadPosition": slot, "playerPosition": "DEF"})
-        elif player["playerPosition"] == "GK":
-            if increment <= 11:
-                slot["position"] = increment
-                slot["soccerPlayerId"] = player["id"]
-                items.append({"squadPosition": slot, "playerPosition": "DEF"})
-    log_er.log_info(f" {items}")
+        if player["playerPosition"] == "FW" and position <= 2:
+            # player_ids.append((player['id'], player['playerPosition']))
+            players.append(get_player_template(position, player['id'], "FW"))
+            position += 1
+        elif player["playerPosition"] == "MID" and 2 < position <= 6:
+            # player_ids.append((player['id'], player['playerPosition']))
+            players.append(get_player_template(position-2, player['id'], "MID"))
+            position += 1
+        elif player["playerPosition"] == "DEF" and 6 < position <= 10:
+            # player_ids.append((player['id'], player['playerPosition']))
+            players.append(get_player_template(position-6, player['id'], "DEF"))
+            position += 1
+        elif player["playerPosition"] == "GK" and position == 11:
+            # player_ids.append((player['id'], player['playerPosition']))
+            players.append(get_player_template(0, player['id'], "GK"))
+            position += 1
+    log_er.log_info(players)
 
 
-def test_choose_strategy():
+def test_get_all_soccer_players_info():
+    """ Get info of all the soccer players. """
+    get_all_soccer_players_info()
+    assert True
+
+
+def test_get_squad_info():
+    """ Get Squad Information. """
+    get_squad_info()
+    assert True
+
+
+def test_get_autofill_players():
     """ After choosing strategy, the affected squad is updated. """
     autofill_4_4_2()
-    # get_all_soccer_player_ids()
-    # get_soccer_players()
-    # get_squad_info()
     assert True
